09a291b07d5a71b6fcf649a5a3c1069d2c7464b8
LP <Wb575869>
2022-08-07 00:27:50 +0800

    init7

--- javaweb_2/src/main/java/com/atguigu/javaweb_2/servlet/HiServlet.java
+++ javaweb_2/src/main/java/com/atguigu/javaweb_2/servlet/HiServlet.java
@@ -1,22 +1,22 @@
 package com.atguigu.javaweb_2.servlet;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+
 /**
  * servlet 的生命周期
- *
+ * <p>
  * 1:执行servlet构造器方法
  * 2：执行init初始化方法
  * 1,2步 是在第一次访问的时候创建servlet程序被调用
- *
+ * <p>
  * 3：执行service 方法
  * 每次访问的时候都会被调用
- *
+ * <p>
  * 4：执行destroy销毁方法
  * web工程停止 的时候被调用
- *
- *
- * */
+ */
 
 public class HiServlet implements Servlet {
     public HiServlet() {
@@ -33,14 +33,39 @@ public class HiServlet implements Servlet {
     public ServletConfig getServletConfig() {
         return null;
     }
-/**
- * service方法专门用来处理请求和响应的
- *
- * */
+
+    /**
+     * service方法专门用来处理请求和响应的
+     */
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
         System.out.println("3：servlet程序被访问了");
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        //获取请求的方式
+        String method = httpServletRequest.getMethod();
+        if ("GET".equals(method)) {
+            doGet();
+        } else if ("POST".equals(method)) {
+            doPost();
+        }
 
+
+    }
+
+    /*
+     *
+     * get请求操作
+     * */
+    public void doGet() {
+        System.out.println("get请求");
+    }
+
+    /*
+     *
+     * post请求操作
+     * */
+    public void doPost() {
+        System.out.println("post请求");
     }
 
     @Override
---
+++ javaweb_2/target/javaweb_2-1.0-SNAPSHOT/index.jsp
@@ -0,0 +1,20 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JSP - Hello World</title>
+</head>
+<body>
+<h1>
+    <%= "Hello World!" %>
+
+</h1>
+这是javaweb 的第一个工程
+<br/>
+热部署测试
+22
+<br/>
+
+<a href="hello-servlet">Hello Servlet</a>
+</body>
+</html>
